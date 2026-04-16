c572368cbd3bfa2b8638ed01b632881baa47fa1b
digitalvibers-cmd <digitalvibe.rs@gmail.com>
2026-04-16 22:09:15 +0200

    style(auth): fix Vendor fully-qualified name in CSR directive

--- packages/fleetops/server/src/Auth/Directives/CustomerOrders.php
+++ packages/fleetops/server/src/Auth/Directives/CustomerOrders.php
@@ -3,6 +3,7 @@
 namespace Fleetbase\FleetOps\Auth\Directives;
 
 use Fleetbase\Contracts\Directive;
+use Fleetbase\FleetOps\Models\Vendor;
 use Illuminate\Database\Eloquent\Builder;
 
 class CustomerOrders implements Directive
@@ -16,7 +17,7 @@ class CustomerOrders implements Directive
             $query->orWhereHas('authenticatableCustomer', function ($query) use ($id) {
                 $query->where('user_uuid', $id);
             });
-            $query->orWhereHasMorph('customer', [\Fleetbase\FleetOps\Models\Vendor::class], function ($query) use ($id) {
+            $query->orWhereHasMorph('customer', [Vendor::class], function ($query) use ($id) {
                 $query->whereHas('personnels', function ($query) use ($id) {
                     $query->where('contacts.user_uuid', $id);
                 });
